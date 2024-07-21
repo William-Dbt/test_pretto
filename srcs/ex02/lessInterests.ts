113a2df6057bf2fb6c48315ccf3dffab5a567655
William Debotte <wdebotte@student.42.fr>
2024-07-21 14:20:03 +0200

    Continue working on ex02 Add typescript to depencendies

--- srcs/ex02/lessInterests.ts
+++ srcs/ex02/lessInterests.ts
@@ -1,5 +1,23 @@
 import amountInterets from "../ex00/amountInterests"
 
-function findLessInterests(loanDuration: number, loanAmount: number) {
+let arr_rates = new Map<number, number>([
+	[10, 2.9],
+	[12, 3.2],
+	[15, 3.5],
+	[20, 3.8],
+	[22, 3.8],
+	[25, 4.4]
+]);
 
+function findLessInterests(loanDuration: number, loanAmount: number) {
+	if (!loanDuration || !loanAmount) {
+		console.error("[findLessInterests] ERROR: at least one parameter is invalid");
+		return ;
+	}
+	if (!arr_rates.get(loanDuration)) {
+		console.error("[findLessInterests] ERROR: can't find a rate for this loan duration (", loanDuration, ')');
+		return ;
+	}
 }
+
+findLessInterests(30, 300000);
